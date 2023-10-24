ab56e8d4bc1c701fb91107ef6b8f313158d46c6c
jsh3418 <albert5428@gmail.com>
2023-10-24 15:28:43 +0900

    feat: 라우트 설정

---
+++ fe/user/src/layouts/BaseLayout/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from 'layouts/BaseLayout/Header';
+import { Outlet } from 'react-router-dom';
+
+export const BaseLayout: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
--- fe/user/src/router.tsx
+++ fe/user/src/router.tsx
@@ -1,3 +1,7 @@
+import { BaseLayout } from 'layouts/BaseLayout';
+import { MainPage } from '@pages/MainPage';
+import { MapPage } from '@pages/MapPage';
+import { InquiryPage } from '@pages/InquiryPage';
 import {
   Route,
   createBrowserRouter,
@@ -6,6 +10,12 @@ import {
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<div>테스트</div>} />,
+    <Route path="/">
+      <Route element={<BaseLayout />}>
+        <Route index element={<MainPage />} />
+        <Route path="map" element={<MapPage />} />
+        <Route path="inquiry" element={<InquiryPage />} />
+      </Route>
+    </Route>,
   ),
 );
