30bc04861d0f661e4a877dfe090da1ab08903edc
jsh3418 <albert5428@gmail.com>
2023-10-25 17:01:00 +0900

    feat: 배너 UI 구현

--- fe/user/src/pages/MainPage/Carousel.tsx
+++ fe/user/src/pages/MainPage/Carousel.tsx
@@ -1,3 +1,13 @@
+import { SwiperComponent } from './SwiperComponent';
+
 export const Carousel: React.FC = () => {
-  return <></>;
+  return (
+    <SwiperComponent
+      imageUrls={[
+        'https://github.com/jsh3418/js-calculator-bonobono/assets/57666791/85b44f33-49b9-46d6-80de-e9a538c44b2f',
+        'https://github.com/jsh3418/js-calculator-bonobono/assets/57666791/85b44f33-49b9-46d6-80de-e9a538c44b2f',
+        'https://github.com/jsh3418/js-calculator-bonobono/assets/57666791/85b44f33-49b9-46d6-80de-e9a538c44b2f',
+      ]}
+    />
+  );
 };
