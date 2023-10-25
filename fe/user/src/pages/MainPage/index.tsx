8e937d8a729d00c9feee99ebf31758b99e952622
jsh3418 <albert5428@gmail.com>
2023-10-25 23:12:12 +0900

    refactor: MainSection 디렉토리 분리 및 컴포넌트 이동

--- fe/user/src/pages/MainPage/index.tsx
+++ fe/user/src/pages/MainPage/index.tsx
@@ -1,23 +1,13 @@
-import styled from '@emotion/styled';
-import { AroundVenus } from './AroundVenus';
 import { Banner } from './Banner';
-import { OngoingShows } from './OngoingShows';
+import { MainSection } from './MainSection';
 import { Footer } from './Footer';
 
 export const MainPage: React.FC = () => {
   return (
     <>
       <Banner />
-      <MainSection>
-        <AroundVenus />
-        <OngoingShows />
-      </MainSection>
+      <MainSection />
       <Footer />
     </>
   );
 };
-
-const MainSection = styled.div`
-  max-width: 1200px;
-  margin: 0 auto 173px auto;
-`;
