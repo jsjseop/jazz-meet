73dc1948f5b2bd6aaf83a87f35422c6944c3bc3a
jello <song6240@naver.com>
2023-10-26 01:27:13 +0900

    feat: 문의 페이지 레이아웃

--- fe/user/src/layouts/BaseLayout/index.tsx
+++ fe/user/src/layouts/BaseLayout/index.tsx
@@ -1,11 +1,17 @@
+import styled from '@emotion/styled';
 import { Header } from 'layouts/BaseLayout/Header';
 import { Outlet } from 'react-router-dom';
 
 export const BaseLayout: React.FC = () => {
   return (
-    <>
+    <BaseLayoutWrapper>
       <Header />
       <Outlet />
-    </>
+    </BaseLayoutWrapper>
   );
 };
+
+const BaseLayoutWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
