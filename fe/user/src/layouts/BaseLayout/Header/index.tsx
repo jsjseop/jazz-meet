1bfc9ad0482e06f6be7602b291c2a92b14763070
jsh3418 <albert5428@gmail.com>
2023-10-25 12:03:46 +0900

    feat: 헤더 로고 추가 및 스타일 추가

--- fe/user/src/layouts/BaseLayout/Header/index.tsx
+++ fe/user/src/layouts/BaseLayout/Header/index.tsx
@@ -5,7 +5,7 @@ export const Header: React.FC = () => {
   return (
     <StyledHeader>
       <LeftContainer>
-        <div>Jazz Meet Logo</div>
+        <LogoImage src="https://github.com/jsh3418/js-calculator-bonobono/assets/57666791/07da94d4-01b3-4e70-8973-db44780f6d6e" />
         <Buttons>
           <button>지도</button>
           <button>문의</button>
@@ -19,15 +19,29 @@ export const Header: React.FC = () => {
 
 const StyledHeader = styled.header`
   width: 100%;
+  height: 73px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  background-color: #000;
 `;
 
 const LeftContainer = styled.div`
   display: flex;
+  gap: 34px;
+`;
+
+const LogoImage = styled.img`
+  width: 146px;
+  height: 27px;
+  object-fit: cover;
 `;
 
 const Buttons = styled.nav`
   display: flex;
+  gap: 34px;
+
+  > button {
+    color: #fff;
+  }
 `;
