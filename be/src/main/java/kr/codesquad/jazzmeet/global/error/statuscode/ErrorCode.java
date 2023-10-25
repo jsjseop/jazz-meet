4f693a35289c9c39a78861f05eb6c89961abc3c9
sudago <sudago22@gmail.com>
2023-10-25 14:31:32 +0900

    fix: 404 에러코드 삭제

--- be/src/main/java/kr/codesquad/jazzmeet/global/error/statuscode/ErrorCode.java
+++ be/src/main/java/kr/codesquad/jazzmeet/global/error/statuscode/ErrorCode.java
@@ -9,8 +9,6 @@ public enum ErrorCode implements StatusCode {
 	// 예시. 필요한 에러가 있을 때 마다 구현체를 추가해서 사용합니다.
 	// 400 Bad Request
 	VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "유효하지 않은 형식입니다."),
-	// 404 Not Found
-	PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "페이지를 찾을 수 없습니다."),
 	// 500 Internal Server Error
 	INTERNAL_SERVER_ERROR_DB(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 에러입니다."),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다.");
