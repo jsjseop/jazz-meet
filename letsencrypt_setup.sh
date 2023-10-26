079c2329aa9aecdbbc798b2cf46736e49ba1149a
hyeseon <chunghye1998@gmail.com>
2023-10-26 20:56:39 +0900

    infra: nginx 실행 안되는 문제 - 이메일 등록

--- letsencrypt_setup.sh
+++ letsencrypt_setup.sh
@@ -9,6 +9,7 @@ fi
 domains="jazzmeet.site"
 rsa_key_size=4096
 data_path="./data/certbot"
+email="chunghye1998@gmail.com"
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 # 기존 인증서가 있으면 사용자에게 덮어 쓸 것인지 질문
@@ -57,12 +58,19 @@ echo "### Requesting Let's Encrypt certificate for $domains ..."
 #Join $domains to -d args
 domain_args="-d $domains"
 
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
+
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
+    $email_arg \
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
