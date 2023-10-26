c44f4cdb70e4c1976a4c6746d9a4e16330805c08
jsh3418 <albert5428@gmail.com>
2023-10-27 08:20:59 +0900

    chore: 공연장 상세보기 더미 사진 수정

--- fe/user/src/pages/MapPage/Panel/VenueDetail/PreviewImages.tsx
+++ fe/user/src/pages/MapPage/Panel/VenueDetail/PreviewImages.tsx
@@ -7,35 +7,35 @@ export const PreviewImages: React.FC = () => {
     {
       id: 1,
       thumbnailUrl:
-        'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20150831_275%2F1441004893369gYh5w_JPEG%2F11611824_0.jpg',
+        'https://github.com/jazz-meet/jazz-meet/assets/57666791/26341399-76b4-40a1-92e1-7380f2cb6968',
       name: 'Evans',
       address: '서울 마포구 와우산로 63 2층',
     },
     {
       id: 2,
       thumbnailUrl:
-        'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20230408_221%2F1680929454542Aovwz_JPEG%2FKakaoTalk_20230214_192728451.jpg',
+        'https://github.com/jazz-meet/jazz-meet/assets/57666791/c9f4058e-d4a2-4539-9691-2694a4415e23',
       name: '부기우기',
       address: '서울 용산구 회나무로 21 2층',
     },
     {
       id: 3,
       thumbnailUrl:
-        'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20210402_140%2F1617367262454YU3Hd_JPEG%2FNsOFazqvYXyXDXceOEjH3cm6.jpg',
+        'https://github.com/jazz-meet/jazz-meet/assets/57666791/bd7f599a-cbb8-4087-8f02-51075c925d44',
       name: '블루밍 재즈바',
       address: '서울 강남구 테헤란로19길 21 지하1층',
     },
     {
       id: 4,
       thumbnailUrl:
-        'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20201118_121%2F1605662603727ABSSF_JPEG%2FKakaoTalk_20200919_012029316.jpg',
+        'https://github.com/jazz-meet/jazz-meet/assets/57666791/0c15127e-4525-4dc3-8aef-21648ce23cd7',
       name: '플랫나인',
       address: '서울 서초구 강남대로65길 10 5층',
     },
     {
       id: 5,
       thumbnailUrl:
-        'https://search.pstatic.net/common/?src=https%3A%2F%2Fldb-phinf.pstatic.net%2F20150831_275%2F1441004893369gYh5w_JPEG%2F11611824_0.jpg',
+        'https://github.com/jazz-meet/jazz-meet/assets/57666791/a9bbfa04-e2e2-4152-90db-c5c45a167692',
       name: 'Evans',
       address: '서울 마포구 와우산로 63 2층',
     },
