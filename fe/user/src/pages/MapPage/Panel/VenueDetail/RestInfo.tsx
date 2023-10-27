ee2d2f456a5ddb180bbe09c09449a2a9ceba7a99
jsh3418 <albert5428@gmail.com>
2023-10-27 11:45:06 +0900

    feat: RestInfo 레이아웃 구현

--- fe/user/src/pages/MapPage/Panel/VenueDetail/RestInfo.tsx
+++ fe/user/src/pages/MapPage/Panel/VenueDetail/RestInfo.tsx
@@ -4,6 +4,7 @@ import { Tabs } from './Tabs';
 import { Tab } from './Tabs/Tab';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import LocalCafeOutlinedIcon from '@mui/icons-material/LocalCafeOutlined';
 
 export const RestInfo: React.FC = () => {
   return (
@@ -50,6 +51,22 @@ export const RestInfo: React.FC = () => {
           </StyledShowListContent>
         </StyledShowList>
       </StyledRestInfoContent>
+
+      <Tabs>
+        <Tab isSelected>공연장정보</Tab>
+      </Tabs>
+      <StyledContentContainer>
+        <StyledContent>
+          <LocalCafeOutlinedIcon
+            sx={{ width: '28px', height: '28px', fill: '#848484' }}
+          />
+          <StyledBasicInfoText>
+            올댓재즈는 1976년부터 한국의 재즈씬을 이끌어온 한국의 대표브랜드로서
+            재즈 매니아들의 성지와도 같은 공간입니다.우리는 재즈의 불모지로
+            불리는 한국에서 재즈 장르의 음악이 조금 더 대중들...
+          </StyledBasicInfoText>
+        </StyledContent>
+      </StyledContentContainer>
     </StyledRestInfo>
   );
 };
@@ -62,6 +79,10 @@ const StyledRestInfoContent = styled.div`
   padding: 22px 54px;
   display: flex;
   gap: 11px;
+
+  @media screen and (max-width: 1500px) {
+    flex-direction: column;
+  }
 `;
 
 const StyledShowList = styled.div`
@@ -69,6 +90,7 @@ const StyledShowList = styled.div`
   padding: 24px;
   border: 1px solid #dbdbdb;
   border-radius: 8px;
+  box-sizing: border-box;
 `;
 
 const StyledShowListHeader = styled.div`
@@ -95,4 +117,25 @@ const StyledShowListIndex = styled.div`
 
 const StyledShowListName = styled.div`
   justify-self: start;
+`;
+
+const StyledContentContainer = styled.div`
+  padding: 24px 30px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledContent = styled.div`
+  display: flex;
+  gap: 19px;
+  align-items: center;
+
+  > svg {
+    align-self: flex-start;
+  }
+`;
+
+const StyledBasicInfoText = styled.div`
+  font-size: 20px;
+  line-height: 150%;
 `;
