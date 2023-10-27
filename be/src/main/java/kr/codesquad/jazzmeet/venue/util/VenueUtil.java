58d701a6fea3d71d5b7264fdaed160a5530da4d0
jsjseop <57559288+jsjseop@users.noreply.github.com>
2023-10-27 23:52:51 +0900

    refactor: createPoint 메서드 수정

--- be/src/main/java/kr/codesquad/jazzmeet/venue/util/VenueUtil.java
+++ be/src/main/java/kr/codesquad/jazzmeet/venue/util/VenueUtil.java
@@ -1,10 +1,8 @@
 package kr.codesquad.jazzmeet.venue.util;
 
 import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.CoordinateSequence;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
-import org.locationtech.jts.geom.impl.CoordinateArraySequence;
 
 public final class VenueUtil {
 
@@ -13,10 +11,7 @@ public final class VenueUtil {
 	}
 
 	public static Point createPoint(Double latitude, Double longitude) {
-		Coordinate[] coordinates = new Coordinate[] {
-			new Coordinate(latitude, longitude)};
-		CoordinateSequence coordinateSequence = new CoordinateArraySequence(coordinates);
 		GeometryFactory geometryFactory = new GeometryFactory();
-		return geometryFactory.createPoint(coordinateSequence);
+		return geometryFactory.createPoint(new Coordinate(longitude, latitude));
 	}
 }
