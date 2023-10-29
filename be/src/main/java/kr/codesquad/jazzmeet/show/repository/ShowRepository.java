4092e386445525ca2c47cf20fc72db2c298b90d9
sudago <sudago22@gmail.com>
2023-10-29 22:40:53 +0900

    refactor: JpaRepository로 변경

--- be/src/main/java/kr/codesquad/jazzmeet/show/repository/ShowRepository.java
+++ be/src/main/java/kr/codesquad/jazzmeet/show/repository/ShowRepository.java
@@ -3,13 +3,13 @@ package kr.codesquad.jazzmeet.show.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import kr.codesquad.jazzmeet.show.entity.Show;
 
 @Repository
-public interface ShowRepository extends CrudRepository<Show, Long> {
+public interface ShowRepository extends JpaRepository<Show, Long> {
 
 	List<Show> findTop10BystartTimeGreaterThanOrEndTimeGreaterThanOrderByStartTime(LocalDateTime BeforeTime,
 		LocalDateTime upcomingTime);
