00f3b9d79ea2cea1f17ce9986fedb35b4ad8e84a
jsh3418 <albert5428@gmail.com>
2023-10-30 23:16:08 +0900

    chore: ShowInfo 디렉토리 분리 및 파일 이동

---
+++ fe/user/src/pages/MapPage/Panel/VenueDetail/RestInfo/ShowInfo/index.tsx
@@ -0,0 +1,113 @@
+import styled from '@emotion/styled';
+import { Calendar } from '../Calendar';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+export const ShowInfo: React.FC = () => {
+  return (
+    <>
+      <Calendar />
+      <StyledShowList>
+        <StyledShowListHeader>
+          <ChevronLeftIcon sx={{ fill: '#B5BEC6' }} />
+          <div>10월 17일 화요일</div>
+          <ChevronRightIcon sx={{ fill: '#B5BEC6' }} />
+        </StyledShowListHeader>
+        <StyledShowListContent>
+          <StyledShowListContentHeader>
+            <div>시작</div>
+            <div>종료</div>
+          </StyledShowListContentHeader>
+
+          <StyledShowListItem>
+            <StyledShowListItemIndex>01</StyledShowListItemIndex>
+            <StyledShowListItemName>권트리오</StyledShowListItemName>
+            <StyledShowListItemTime>12:00</StyledShowListItemTime>
+            <StyledShowListItemTime>14:00</StyledShowListItemTime>
+          </StyledShowListItem>
+
+          <StyledShowListItem>
+            <StyledShowListItemIndex>02</StyledShowListItemIndex>
+            <StyledShowListItemName>김철수 쿼텟</StyledShowListItemName>
+            <StyledShowListItemTime>12:00</StyledShowListItemTime>
+            <StyledShowListItemTime>14:00</StyledShowListItemTime>
+          </StyledShowListItem>
+
+          <StyledShowListItem>
+            <StyledShowListItemIndex>03</StyledShowListItemIndex>
+            <StyledShowListItemName>
+              WE 필하모닉스의 피아노 퀀텟
+            </StyledShowListItemName>
+            <StyledShowListItemTime>12:00</StyledShowListItemTime>
+            <StyledShowListItemTime>14:00</StyledShowListItemTime>
+          </StyledShowListItem>
+
+          <StyledShowListItem>
+            <StyledShowListItemIndex>04</StyledShowListItemIndex>
+            <StyledShowListItemName>러셀 말론 솔로 기타</StyledShowListItemName>
+            <StyledShowListItemTime>12:00</StyledShowListItemTime>
+            <StyledShowListItemTime>14:00</StyledShowListItemTime>
+          </StyledShowListItem>
+        </StyledShowListContent>
+      </StyledShowList>
+    </>
+  );
+};
+
+const StyledShowList = styled.div`
+  width: 100%;
+  padding: 24px;
+  border: 1px solid #dbdbdb;
+  border-radius: 8px;
+  box-sizing: border-box;
+`;
+
+const StyledShowListHeader = styled.div`
+  font-size: 14px;
+  margin-bottom: 22px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledShowListContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+const StyledShowListContentHeader = styled.div`
+  display: flex;
+  justify-content: end;
+  gap: 14px;
+
+  > div {
+    width: 40px;
+    text-align: center;
+  }
+`;
+
+const StyledShowListItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 14px;
+`;
+
+const StyledShowListItemIndex = styled.div`
+  width: 40px;
+  background-color: #d9d9d9;
+  padding: 4px 8px;
+  border-radius: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledShowListItemName = styled.div`
+  width: 100%;
+  justify-self: start;
+`;
+
+const StyledShowListItemTime = styled.div`
+  width: 40px;
+`;
