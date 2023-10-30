72a5d4f655dfa8cf9018c970df994a669f140899
jsh3418 <albert5428@gmail.com>
2023-10-30 11:13:04 +0900

    chore: 공연장 사진 상세보기 주석처리

--- fe/user/src/pages/MapPage/Panel/VenueDetail/Images/index.tsx
+++ fe/user/src/pages/MapPage/Panel/VenueDetail/Images/index.tsx
@@ -1,12 +1,12 @@
-import { createPortal } from 'react-dom';
+// import { createPortal } from 'react-dom';
 import { PreviewImages } from './PreviewImages';
-import { ImageDetail } from './ImageDetail';
+// import { ImageDetail } from './ImageDetail';
 
 export const Images: React.FC = () => {
   return (
     <>
       <PreviewImages />
-      {createPortal(<ImageDetail />, document.body)}
+      {/* {createPortal(<ImageDetail />, document.body)} */}
     </>
   );
 };
