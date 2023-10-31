64596cbe434a950119ade7daebc8f6f16988c60b
jsh3418 <albert5428@gmail.com>
2023-11-01 00:37:32 +0900

    refactor: Calendar 컴포넌트 디렉토리 제거 및 이동

--- fe/user/src/pages/MapPage/Panel/VenueDetail/RestInfo/ShowInfo/Calendar.tsx
+++ fe/user/src/pages/MapPage/Panel/VenueDetail/RestInfo/ShowInfo/Calendar.tsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { getFirstDay, getLastDay } from './getFirstDayAndLastDay';
 
 type Props = {
   calendarDate: Date;
@@ -74,6 +73,19 @@ export const Calendar: React.FC<Props> = ({
       </StyledDaysGrid>
     </StyledCalendar>
   );
+};
+
+const getFirstDay = (year: number, month: number) => {
+  // month는 0부터 시작하기 때문에 실제 월을 계산할 때는 +1을 해줍니다.
+  const firstDay = new Date(year, month - 1, 1);
+
+  return firstDay.getDay(); // 요일을 숫자로 반환 (0: 일요일, 1: 월요일, ..., 6: 토요일)
+};
+
+const getLastDay = (year: number, month: number) => {
+  const lastDay = new Date(year, month, 0); // 0일은 지난 달의 마지막 날을 의미합니다.
+
+  return lastDay.getDate(); // 마지막 날짜를 가져옵니다.
 };
 
 const StyledCalendar = styled.div`
