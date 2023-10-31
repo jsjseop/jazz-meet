20d8f7c7cb0f54b508961c66c3cbc5743df76a85
hyeseon <chunghye1998@gmail.com>
2023-10-31 19:20:28 +0900

    fix: VenueHour enumerated 추가, show team name 길이 수정

--- be/src/main/java/kr/codesquad/jazzmeet/venue/entity/VenueHour.java
+++ be/src/main/java/kr/codesquad/jazzmeet/venue/entity/VenueHour.java
@@ -2,6 +2,8 @@ package kr.codesquad.jazzmeet.venue.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +21,7 @@ public class VenueHour {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Enumerated(value = EnumType.ORDINAL)
 	@Column(nullable = false, length = 10)
 	private DayOfWeek day;
 	@Column(nullable = false, length = 20)
