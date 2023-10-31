d1dfebcccd7fd398781217afc1c78f5b5e333829
jsh3418 <albert5428@gmail.com>
2023-11-01 00:35:13 +0900

    chore: useCalendar 주석추가

--- fe/user/src/pages/MapPage/Panel/VenueDetail/RestInfo/ShowInfo/useCalendar.ts
+++ fe/user/src/pages/MapPage/Panel/VenueDetail/RestInfo/ShowInfo/useCalendar.ts
@@ -5,13 +5,13 @@ export const useCalendar = () => {
   const [selectedDate, setSelectedDate] = useState(() => new Date());
 
   const goToPreviousMonth = () => {
-    calendarDate.setDate(1);
+    calendarDate.setDate(1); // calendarDate가 31일일 때, 2개월 넘어가는 현상 방지
     calendarDate.setMonth(calendarDate.getMonth() - 1);
     setCalendarDate(new Date(calendarDate));
   };
 
   const goToNextMonth = () => {
-    calendarDate.setDate(1);
+    calendarDate.setDate(1); // calendarDate가 31일일 때, 2개월 넘어가는 현상 방지
     calendarDate.setMonth(calendarDate.getMonth() + 1);
     setCalendarDate(new Date(calendarDate));
   };
