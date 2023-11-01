225ff7ed7a8d3696b9c9a54d39d5f649f2c4d708
hyeseon <chunghye1998@gmail.com>
2023-11-01 15:39:50 +0900

    feat: 공연장 위치 정보 조회(핀) - 지도기반 API 구현, 검색 API, dto, vo 네이밍 변경

--- be/src/main/java/kr/codesquad/jazzmeet/venue/util/VenueUtil.java
+++ be/src/main/java/kr/codesquad/jazzmeet/venue/util/VenueUtil.java
@@ -3,6 +3,7 @@ package kr.codesquad.jazzmeet.venue.util;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.Polygon;
 
 import kr.codesquad.jazzmeet.global.error.CustomException;
 import kr.codesquad.jazzmeet.global.error.statuscode.LocationErrorCode;
@@ -20,5 +21,20 @@ public final class VenueUtil {
 		} catch (Exception e) {
 			throw new CustomException(LocationErrorCode.INVALID_LOCATION_ERROR);
 		}
+	}
+
+	// 좌표를 사용하여 사각형을 생성합니다.
+	public static Polygon createRange(Double lowLatitude, Double highLatitude, Double lowLongitude,
+		Double highLongitude) {
+		GeometryFactory geometryFactory = new GeometryFactory();
+		Coordinate[] coordinates = new Coordinate[] {
+			new Coordinate(lowLongitude, lowLatitude),
+			new Coordinate(highLongitude, lowLatitude),
+			new Coordinate(highLongitude, highLatitude),
+			new Coordinate(lowLongitude, highLatitude),
+			new Coordinate(lowLongitude, lowLatitude)
+		};
+
+		return geometryFactory.createPolygon(coordinates);
 	}
 }
