6e300e668538889f0256c883bc66f7393378c3ee
jello <song6240@naver.com>
2023-11-02 12:07:40 +0900

    feat: 지도 이동 시 현 위치에서 검색 버튼 띄우기

--- fe/user/src/pages/MapPage/Map/index.tsx
+++ fe/user/src/pages/MapPage/Map/index.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
-import { useCallback, useEffect } from 'react';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import { useCallback, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { getVenuePinsBySearch } from '~/apis/venue';
 import { BASIC_COORDINATE } from '~/constants/COORDINATE';
@@ -14,6 +15,7 @@ type Props = {
 export const Map: React.FC<Props> = ({ mapRef }) => {
   const { search: searchQueryString } = useLocation();
   const { userCoordinate } = useUserCoordinate();
+  const [isBoundsChanged, setIsBoundsChanged] = useState(false);
 
   const getInitMap = useCallback(() => {
     const initCoordinate = userCoordinate ?? BASIC_COORDINATE;
@@ -30,9 +32,13 @@ export const Map: React.FC<Props> = ({ mapRef }) => {
     const updateView = async () => {
       const map = getInitMap();
 
-      naver.maps.Event.addListener(map, 'bounds_changed', debounce(() => {
-        console.log("bounds_changed");
-      }, 100))
+      naver.maps.Event.addListener(
+        map,
+        'bounds_changed',
+        debounce(() => {
+          setIsBoundsChanged(true);
+        }, 100),
+      );
 
       if (!searchQueryString) {
         return;
@@ -47,10 +53,38 @@ export const Map: React.FC<Props> = ({ mapRef }) => {
     updateView();
   }, [searchQueryString, userCoordinate, getInitMap]);
 
-  return <StyledMap id="map" ref={mapRef} />;
+  return (
+    <StyledMap id="map" ref={mapRef}>
+      {isBoundsChanged && (
+        <StyledButton>
+          <RefreshIcon />
+          <span>현 지도에서 검색</span>
+        </StyledButton>
+      )}
+    </StyledMap>
+  );
 };
 
 const StyledMap = styled.div`
   width: 100%;
   height: inherit;
+`;
+
+const StyledButton = styled.button`
+  position: absolute;
+  bottom: 8%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 1000;
+  padding: 12px 16px;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  gap: 9px;
+  color: #fff;
+  background-color: #0775f4;
+
+  &:hover {
+    background-color: #0a84ff;
+  }
 `;
