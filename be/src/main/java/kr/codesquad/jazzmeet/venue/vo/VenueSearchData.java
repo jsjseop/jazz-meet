f397ddefdf6b91ea55caddb08ecc40c822c837be
jsjseop <57559288+jsjseop@users.noreply.github.com>
2023-11-02 14:49:42 +0900

    refactor: 생성자 어노테이션 제거

--- be/src/main/java/kr/codesquad/jazzmeet/venue/vo/VenueSearchData.java
+++ be/src/main/java/kr/codesquad/jazzmeet/venue/vo/VenueSearchData.java
@@ -5,13 +5,9 @@ import java.util.List;
 import org.locationtech.jts.geom.Point;
 
 import kr.codesquad.jazzmeet.venue.dto.ShowInfo;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class VenueSearchData {
 	private Long id;
 	private String thumbnailUrl;
