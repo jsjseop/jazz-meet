12cc2cf6715b0fe601a639fa2aa15a3845634524
hyeseon <chunghye1998@gmail.com>
2023-11-10 18:12:32 +0900

    feat: 공연 목록 조회 API 구현

---
+++ be/src/main/java/kr/codesquad/jazzmeet/show/dto/ShowResponse.java
@@ -0,0 +1,15 @@
+package kr.codesquad.jazzmeet.show.dto;
+
+import java.util.List;
+
+import kr.codesquad.jazzmeet.show.vo.ShowSummaryWithVenue;
+import lombok.Builder;
+
+@Builder
+public record ShowResponse(
+	List<ShowSummaryWithVenue> shows,
+	long totalCount,
+	int currentPage,
+	int maxPage
+) {
+}
