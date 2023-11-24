e6aa4afe6aedb57537f78a88af7ceb24ed60fe76
jello <song6240@naver.com>
2023-11-24 15:23:13 +0900

    feat: 오늘 영업시간 노출

--- fe/user/src/pages/MapPage/Panel/VenueDetail/BasicInfo/VenueHours.tsx
+++ fe/user/src/pages/MapPage/Panel/VenueDetail/BasicInfo/VenueHours.tsx
@@ -1,21 +1,27 @@
 import styled from '@emotion/styled';
 import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
 import { VenueHour } from '~/types/api.types';
+import { getKoreanWeekdayName } from '~/utils/dateUtils';
 
 type Props = {
   venueHours: VenueHour[];
 };
 
 export const VenueHours: React.FC<Props> = ({ venueHours }) => {
+  const today = getKoreanWeekdayName(new Date().getDay()) + '요일';
+  const todayBusinessHours = venueHours.find(
+    (venueHour) => venueHour.day === today,
+  )?.businessHours;
+
   return (
     <StyledVenueHours>
       <StyledTodayBusinessHours>
-        영업시간
+        {todayBusinessHours || '영업시간 정보가 없습니다.'}
         <ExpandMoreOutlinedIcon />
       </StyledTodayBusinessHours>
 
       {venueHours.map((venueHour, index) => (
-        <StyledBasicInfoText key={index}>
+        <StyledBasicInfoText key={index} $highlight={today === venueHour.day}>
           {`${venueHour.day} | ${venueHour.businessHours}`}
         </StyledBasicInfoText>
       ))}
@@ -39,8 +45,9 @@ const StyledTodayBusinessHours = styled.summary`
   }
 `;
 
-const StyledBasicInfoText = styled.div`
+const StyledBasicInfoText = styled.div<{ $highlight: boolean }>`
   height: 32px;
   display: flex;
   align-items: center;
+  font-weight: ${({ $highlight }) => ($highlight ? 'bold' : 'normal')};
 `;
