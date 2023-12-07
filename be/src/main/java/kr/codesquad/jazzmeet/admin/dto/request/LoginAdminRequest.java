9f9f04231c590e746317398e5f6c9c24e8ae02d6
hyeseon <chunghye1998@gmail.com>
2023-12-07 17:25:58 +0900

    test: 관리자 계정 로그인 Controller test 작성

--- be/src/main/java/kr/codesquad/jazzmeet/admin/dto/request/LoginAdminRequest.java
+++ be/src/main/java/kr/codesquad/jazzmeet/admin/dto/request/LoginAdminRequest.java
@@ -2,7 +2,9 @@ package kr.codesquad.jazzmeet.admin.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
+@Builder
 public record LoginAdminRequest (
 	@NotNull
 	@Size(min = 5, max = 20, message = "아이디는 5자 이상, 20자 이하여야 합니다.")
