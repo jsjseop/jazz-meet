82ce2c8b752a466c5cc28d29b9fc7f9d8ad6dfd6
jsh3418 <albert5428@gmail.com>
2023-12-20 14:44:27 +0900

    fix: 첫 렌더링시 디바이스 타입 체크 수정

--- fe/user/src/components/DeviceTypeChecker.tsx
+++ fe/user/src/components/DeviceTypeChecker.tsx
@@ -1,23 +1,17 @@
 import { useEffect } from 'react';
 import { useDeviceTypeStore } from '~/stores/useDeviceTypeStore';
 
-const MOBILE_MAX_WIDTH = 1023;
-
 export const DeviceTypeChecker: React.FC = () => {
-  const { setDeviceType } = useDeviceTypeStore();
+  const updateDeviceType = useDeviceTypeStore(
+    (state) => state.updateDeviceType,
+  );
 
   useEffect(() => {
-    const handleResize = () => {
-      const { innerWidth } = window;
-      const isMobile = innerWidth <= MOBILE_MAX_WIDTH;
-
-      setDeviceType({ isMobile });
-    };
-
-    window.addEventListener('resize', handleResize);
+    updateDeviceType();
+    window.addEventListener('resize', updateDeviceType);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('resize', updateDeviceType);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
