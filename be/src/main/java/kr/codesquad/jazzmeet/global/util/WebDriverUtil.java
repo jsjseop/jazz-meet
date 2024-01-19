c07c4a347670328a5eef86ab392e1b1e325e0deb
sudago <sudago22@gmail.com>
2024-01-19 17:39:10 +0900

    refactor: 웹 드라이버 자동 설치 및 자동 버전 업데이트

--- be/src/main/java/kr/codesquad/jazzmeet/global/util/WebDriverUtil.java
+++ be/src/main/java/kr/codesquad/jazzmeet/global/util/WebDriverUtil.java
@@ -5,27 +5,19 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.RequiredArgsConstructor;
 
-/*
-	- 크롬 드라이버 생성, 크롬 옵션 설정, 크롬 드라이버의 설치 경로 WEB_DRIVER_PATH 설정
-	‼️ 크롬과 드라이버의 버전이 동일해야 한다
- */
 @RequiredArgsConstructor
 @Component
 public class WebDriverUtil {
-	private static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
-	private static String WEB_DRIVER_PATH; // WebDriver 경로
 
 	public static WebDriver getChromeDriver() {
-		// webDriver 설정
-		if (ObjectUtils.isEmpty(System.getProperty(WEB_DRIVER_ID))) {
-			System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
-		}
+		/* 웹 드라이버 최신으로 설치.
+		(‼️ 크롬과 드라이버의 버전이 동일해야 한다.) */
+		WebDriverManager.chromedriver().setup();
 		// 크롬 설정을 담은 객체 생성
 		ChromeOptions chromeOptions = new ChromeOptions();
 		// 브라우저가 눈에 보이지 않고 내부적으로 실행된다.
@@ -44,20 +36,11 @@ public class WebDriverUtil {
 		return driver;
 	}
 
-	@Value("${driver.chrome.driver.path}")
-	public void setWebDriverPath(String path) {
-		WEB_DRIVER_PATH = path;
-	}
-
 	public static void quit(WebDriver driver) {
-		if (!ObjectUtils.isEmpty(driver)) {
-			driver.quit();
-		}
+		driver.quit();
 	}
 
 	public static void close(WebDriver driver) {
-		if (!ObjectUtils.isEmpty(driver)) {
-			driver.close();
-		}
+		driver.close();
 	}
 }
