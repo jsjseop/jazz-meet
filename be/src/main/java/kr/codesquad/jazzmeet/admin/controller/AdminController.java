8249a0582f3ce5cf3f62d3a1b07b237667a02994
hyeseon <chunghye1998@gmail.com>
2024-02-25 19:28:50 +0900

    fix: reissue 요청 방식 변경

--- be/src/main/java/kr/codesquad/jazzmeet/admin/controller/AdminController.java
+++ be/src/main/java/kr/codesquad/jazzmeet/admin/controller/AdminController.java
@@ -9,19 +9,21 @@ import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import kr.codesquad.jazzmeet.admin.AdminMapper;
 import kr.codesquad.jazzmeet.admin.dto.request.LoginAdminRequest;
-import kr.codesquad.jazzmeet.admin.dto.request.ReissueAdminRequest;
 import kr.codesquad.jazzmeet.admin.dto.request.SignUpAdminRequest;
 import kr.codesquad.jazzmeet.admin.dto.response.LoginAdminResponse;
 import kr.codesquad.jazzmeet.admin.entity.Admin;
 import kr.codesquad.jazzmeet.admin.service.AdminService;
+import kr.codesquad.jazzmeet.global.error.CustomException;
+import kr.codesquad.jazzmeet.global.error.statuscode.AdminErrorCode;
 import kr.codesquad.jazzmeet.global.jwt.Jwt;
 import kr.codesquad.jazzmeet.global.jwt.JwtProperties;
 import kr.codesquad.jazzmeet.global.permission.AdminAuth;
 import kr.codesquad.jazzmeet.global.permission.Permission;
-import kr.codesquad.jazzmeet.global.util.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -70,13 +72,26 @@ public class AdminController {
 	 * 관리자 토큰 재발급 API
 	 */
 	@PostMapping("/api/admins/reissue")
-	public ResponseEntity<LoginAdminResponse> reissueToken(@RequestBody @Valid ReissueAdminRequest reissueAdminRequest) {
+	public ResponseEntity<LoginAdminResponse> reissueToken(HttpServletRequest request) {
 
-		Jwt jwt = adminService.reissue(reissueAdminRequest);
+		String refreshToken = extractRefreshToken(request.getCookies());
+
+		Jwt jwt = adminService.reissue(refreshToken);
 
 		return ResponseEntity.ok()
 			.header(HttpHeaders.SET_COOKIE, getRefreshToken(jwt).toString())
 			.body(AdminMapper.INSTANCE.toLoginAdminResponse(jwt));
+	}
+
+	private String extractRefreshToken(Cookie[] cookies) {
+		if (cookies == null)
+			throw new CustomException(AdminErrorCode.NOT_EXIST_COOKIE);
+		for (Cookie cookie : cookies) {
+			if ("refreshToken".equals(cookie.getName())) {
+				return cookie.getValue();
+			}
+		}
+		throw new CustomException(AdminErrorCode.NOT_FOUND_TOKEN_COOKIE);
 	}
 
 	/**
